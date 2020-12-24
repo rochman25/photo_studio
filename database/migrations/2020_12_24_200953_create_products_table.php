d029febc91e0f70c306fd3daf5b9469d53f50945
rochman25 <zaenur.rochman98@gmail.com>
2020-12-25 01:16:11 +0700

    update migration

---
+++ database/migrations/2020_12_24_200953_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('nama',150);
+            $table->text('deskripsi');
+            $table->double('harga');
+            $table->double('diskon')->nullable(true);
+            $table->string('thumbnail');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('product_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
