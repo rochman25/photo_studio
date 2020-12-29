01550e44c72f702e4cff3b914f70e16264712f42
rochman25 <zaenur.rochman98@gmail.com>
2020-12-30 04:00:48 +0700

    add assets and init layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<!-- begin::Head -->
+
+<head>
+    <base href="">
+    <meta charset="utf-8" />
+    <title>Metronic | Dashboard</title>
+    <meta name="description" content="Updates and statistics">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--begin::Fonts -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Roboto:300,400,500,600,700">
+
+    <!--end::Fonts -->
+
+    <!--begin::Page Vendors Styles(used by this page) -->
+    <link href="{{ asset('admin_asset/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
+        type="text/css" />
+
+    <!--end::Page Vendors Styles -->
+
+    <!--begin::Global Theme Styles(used by all pages) -->
+    <link href="{{ asset('admin_asset/assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin_asset/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+
+    <!--end::Global Theme Styles -->
+
+    <!--begin::Layout Skins(used by all pages) -->
+    <link href="{{ asset('admin_asset/assets/css/skins/header/base/light.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin_asset/assets/css/skins/header/menu/light.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin_asset/assets/css/skins/brand/dark.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin_asset/assets/css/skins/aside/dark.css') }}" rel="stylesheet" type="text/css" />
+    
+    <!--end::Layout Skins -->
+    <link rel="shortcut icon" href="{{ asset('admin_asset/assets/media/logos/favicon.ico') }}" />
+    @stack('styles')
+</head>
+
+<!-- end::Head -->
+
+<!-- begin::Body -->
+
+<body
+    class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--enabled kt-subheader--fixed kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading">
+    @include('partials.header-mobile')
+
+    <div class="kt-grid kt-grid--hor kt-grid--root">
+        <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page">
+            @include('partials.sidebar')
+
+            <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper" id="kt_wrapper">
+                <div id="kt_header" class="kt-header kt-grid__item  kt-header--fixed ">
+                    @include('partials.navbar')
+                </div>
+                <div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">
+                    @yield('pages')
+                </div>
+                <div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
+                    @include('partials.footer')
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- begin::Global Config(global config for global JS sciprts) -->
+    <script>
+        var KTAppOptions = {
+            "colors": {
+                "state": {
+                    "brand": "#5d78ff",
+                    "dark": "#282a3c",
+                    "light": "#ffffff",
+                    "primary": "#5867dd",
+                    "success": "#34bfa3",
+                    "info": "#36a3f7",
+                    "warning": "#ffb822",
+                    "danger": "#fd3995"
+                },
+                "base": {
+                    "label": [
+                        "#c5cbe3",
+                        "#a1a8c3",
+                        "#3d4465",
+                        "#3e4466"
+                    ],
+                    "shape": [
+                        "#f0f3ff",
+                        "#d9dffa",
+                        "#afb4d4",
+                        "#646c9a"
+                    ]
+                }
+            }
+        };
+
+    </script>
+
+    <!-- end::Global Config -->
+
+    <!--begin::Global Theme Bundle(used by all pages) -->
+    <script src="{{ asset('admin_asset/assets/plugins/global/plugins.bundle.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('admin_asset/assets/js/scripts.bundle.js') }}" type="text/javascript"></script>
+
+    <!--end::Global Theme Bundle -->
+
+    <!--begin::Page Vendors(used by this page) -->
+    <script src="{{ asset('admin_asset/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"
+        type="text/javascript"></script>
+    <script src="//maps.google.com/maps/api/js?key=AIzaSyBTGnKT7dt597vo9QgeQ7BFhvSRP4eiMSM" type="text/javascript">
+    </script>
+    <script src="{{ asset('admin_asset/assets/plugins/custom/gmaps/gmaps.js') }}" type="text/javascript"></script>
+
+    <!--end::Page Vendors -->
+
+    <!--begin::Page Scripts(used by this page) -->
+    <script src="{{ asset('admin_asset/assets/js/pages/dashboard.js') }}" type="text/javascript"></script>
+    @stack('scripts')
+    <!--end::Page Scripts -->
+</body>
+
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="kt-container  kt-container--fluid ">
+    <div class="kt-footer__copyright">
+        2019&nbsp;&copy;&nbsp;<a href="http://keenthemes.com/metronic" target="_blank" class="kt-link">Keenthemes</a>
+    </div>
+    <div class="kt-footer__menu">
+        <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">About</a>
+        <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Team</a>
+        <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Contact</a>
+    </div>
+</div>
