2c1319b6a73d1cd8a26bc4d62b26c82f9d919b8b
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-03-27 17:56:23 +0700

    init etc pages
     - update portfolio pages
     - update about us pages

--- resources/views/pages/user/etc/about_us.blade.php
+++ resources/views/pages/user/etc/about_us.blade.php
@@ -16,5 +16,79 @@
 
             </div>
         </section><!-- End Breadcrumbs Section -->
+
+        <section class="portfolio-details">
+            <div class="">
+                <!-- ======= Team Section ======= -->
+                <div id="team">
+                    <div class="container" data-aos="fade-up">
+                        <div class="section-header">
+                            <h3 class="section-title">Team</h3>
+                            <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                                accusantium doloremque</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6">
+                                <div class="member" data-aos="fade-up" data-aos-delay="100">
+                                    <div class="pic"><img src="{{ asset('user_assets/img/team-1.jpg') }}" alt=""></div>
+                                    <h4>Walter White</h4>
+                                    <span>Chief Executive Officer</span>
+                                    <div class="social">
+                                        <a href=""><i class="fa fa-twitter"></i></a>
+                                        <a href=""><i class="fa fa-facebook"></i></a>
+                                        <a href=""><i class="fa fa-google-plus"></i></a>
+                                        <a href=""><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6">
+                                <div class="member" data-aos="fade-up" data-aos-delay="200">
+                                    <div class="pic"><img src="{{ asset('user_assets/img/team-2.jpg') }}" alt=""></div>
+                                    <h4>Sarah Jhinson</h4>
+                                    <span>Product Manager</span>
+                                    <div class="social">
+                                        <a href=""><i class="fa fa-twitter"></i></a>
+                                        <a href=""><i class="fa fa-facebook"></i></a>
+                                        <a href=""><i class="fa fa-google-plus"></i></a>
+                                        <a href=""><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6">
+                                <div class="member" data-aos="fade-up" data-aos-delay="300">
+                                    <div class="pic"><img src="{{ asset('user_assets/img/team-3.jpg') }}" alt=""></div>
+                                    <h4>William Anderson</h4>
+                                    <span>CTO</span>
+                                    <div class="social">
+                                        <a href=""><i class="fa fa-twitter"></i></a>
+                                        <a href=""><i class="fa fa-facebook"></i></a>
+                                        <a href=""><i class="fa fa-google-plus"></i></a>
+                                        <a href=""><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6">
+                                <div class="member" data-aos="fade-up" data-aos-delay="400">
+                                    <div class="pic"><img src="{{ asset('user_assets/img/team-4.jpg') }}" alt=""></div>
+                                    <h4>Amanda Jepson</h4>
+                                    <span>Accountant</span>
+                                    <div class="social">
+                                        <a href=""><i class="fa fa-twitter"></i></a>
+                                        <a href=""><i class="fa fa-facebook"></i></a>
+                                        <a href=""><i class="fa fa-google-plus"></i></a>
+                                        <a href=""><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div><!-- End Team Section -->
+            </div>
+        </section>
+
     </main>
 @endsection
--- resources/views/pages/user/etc/portfolio.blade.php
+++ resources/views/pages/user/etc/portfolio.blade.php
@@ -1,20 +1,104 @@
 @extends('layouts.app_user')
 @section('user_pages')
-<main id="main">
+    <main id="main">
 
-    <!-- ======= Breadcrumbs Section ======= -->
-    <section class="breadcrumbs">
-        <div class="container">
-
-            <div class="d-flex justify-content-between align-items-center">
-                <h2>Portfolio</h2>
-                <ol>
-                    <li><a href="{{ route('view.user.home') }}">Home</a></li>
-                    <li>Portfolio</li>
-                </ol>
+        <!-- ======= Breadcrumbs Section ======= -->
+        <section class="breadcrumbs">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2>Portfolio</h2>
+                    <ol>
+                        <li><a href="{{ route('view.user.home') }}">Home</a></li>
+                        <li>Portfolio</li>
+                    </ol>
+                </div>
             </div>
+        </section><!-- End Breadcrumbs Section -->
 
-        </div>
-    </section><!-- End Breadcrumbs Section -->
-</main>
+        <section class="portfolio-details">
+            <div class="">
+                <!-- ======= Portfolio Section ======= -->
+                <div id="portfolio" class="portfolio">
+                    <div class="container" data-aos="fade-up">
+                        {{-- <div class="row" data-aos="fade-up" data-aos-delay="100">
+                            <div class="col-lg-12 d-flex justify-content-center">
+                                <ul id="portfolio-flters">
+                                    <li data-filter="*" class="filter-active">All</li>
+                                    <li data-filter=".filter-app">App</li>
+                                    <li data-filter=".filter-card">Card</li>
+                                    <li data-filter=".filter-web">Web</li>
+                                </ul>
+                            </div>
+                        </div> --}}
+
+                        <div class="row" data-aos="fade-up" data-aos-delay="200">
+
+                            <div class="col-lg-12 col-md-12 portfolio-item filter-app">
+                                <img src="{{ asset('img/portfolio_1.png') }}" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>App 1</h4>
+                                    <p>App</p>
+                                    <a href="{{ asset('img/category_1.png') }}" data-gall="portfolioGallery"
+                                        class="venobox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+                                    <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                            class="bx bx-link"></i></a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-md-12 portfolio-item filter-web">
+                                <img src="{{ asset('img/portfolio_1.png') }}" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Web 3</h4>
+                                    <p>Web</p>
+                                    <a href="{{ asset('img/portfolio_1.png') }}" data-gall="portfolioGallery"
+                                        class="venobox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+                                    <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                            class="bx bx-link"></i></a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-md-12 portfolio-item filter-app">
+                                <img src="{{ asset('img/portfolio_1.png') }}" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>App 2</h4>
+                                    <p>App</p>
+                                    <a href="{{ asset('img/portfolio_1.png') }}" data-gall="portfolioGallery"
+                                        class="venobox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
+                                    <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                            class="bx bx-link"></i></a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-md-12 portfolio-item filter-card">
+                                <img src="{{ asset('img/portfolio_1.png') }}" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Card 2</h4>
+                                    <p>Card</p>
+                                    <a href="{{ asset('img/portfolio_1.png') }}" data-gall="portfolioGallery"
+                                        class="venobox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
+                                    <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                            class="bx bx-link"></i></a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12 col-md-12 portfolio-item filter-web">
+                                <img src="{{ asset('img/portfolio_1.png') }}" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <h4>Web 2</h4>
+                                    <p>Web</p>
+                                    <a href="{{ asset('img/category_1.png') }}" data-gall="portfolioGallery"
+                                        class="venobox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
+                                    <a href="portfolio-details.html" class="details-link" title="More Details"><i
+                                            class="bx bx-link"></i></a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div><!-- End Portfolio Section -->
+            </div>
+        </section>
+
+    </main>
 @endsection
