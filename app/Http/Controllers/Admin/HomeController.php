a74e01cd8fab6d27beb2ed7b5f48fe716da3647d
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-07-14 21:47:09 +0700

    add dashboard user and admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,6 +12,19 @@ class HomeController extends Controller
 {
     //
     public function index(){
-        return view('pages.admin.dashboard');
+        $user = Auth::user();
+        if($user->role->role->name != "super_admin"){
+            $cancel_order = Booking::where('status','cancel')->where('user_id',$user->id)->count();
+            $pending_order = Booking::where('status','pending')->where('user_id',$user->id)->count(); 
+            $accept_order = Booking::where('status','acc')->where('user_id',$user->id)->count();
+            return view('pages.admin.dashboard-user',compact('cancel_order','pending_order','accept_order'));   
+        }
+        $cancel_order = Booking::where('status','cancel')->count();
+        $pending_order = Booking::where('status','pending')->count(); 
+        $accept_order = Booking::where('status','acc')->count();
+        $best_sellers = [];
+        $new_users = User::orderBY('id','desc')->take(10)->get();
+        $new_orders = Booking::orderBy('id','desc')->take(10)->get();
+        return view('pages.admin.dashboard',compact('cancel_order','pending_order','accept_order','new_orders','new_users','best_sellers'));
     }
 }
