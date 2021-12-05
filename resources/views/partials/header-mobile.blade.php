396c82d54c6ba3051af1ae9b62d68a66175a4da1
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-12-05 17:49:51 +0700

    fix title in mobile page
    Close #27

--- resources/views/partials/header-mobile.blade.php
+++ resources/views/partials/header-mobile.blade.php
@@ -1,14 +1,14 @@
 <!-- begin:: Header Mobile -->
 <div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed ">
     <div class="kt-header-mobile__logo">
-        <a href="index.html">
-            <img alt="Logo" src="{{ asset('admin_asset/assets/media/logos/logo-light.png') }}" />
+        <a href="{{ route('view.home') }}" style="color: white">
+            {{ strtoupper($personalisasi[0]['P001']['value']) }}
+            {{-- <img alt="Logo" src="{{ asset('admin_asset/assets/media/logos/logo-light.png') }}" /> --}}
         </a>
     </div>
     <div class="kt-header-mobile__toolbar">
         <button class="kt-header-mobile__toggler kt-header-mobile__toggler--left"
             id="kt_aside_mobile_toggler"><span></span></button>
-        <button class="kt-header-mobile__toggler" id="kt_header_mobile_toggler"><span></span></button>
         <button class="kt-header-mobile__topbar-toggler" id="kt_header_mobile_topbar_toggler"><i
                 class="flaticon-more"></i></button>
     </div>
